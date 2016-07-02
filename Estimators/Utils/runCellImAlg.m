1b03bb30daa27c8e7ff065da26c418e7661d40a7
uglyculture <archee.noise@gmail.com>
2016-07-02 16:50:10 +0200

    refactoring

--- Estimators/Utils/runCellImAlg.m
+++ Estimators/Utils/runCellImAlg.m
@@ -1,9 +1,9 @@
 function scatter = runCellImAlg( alg, modProj, modFF, cellWinCache )
 %RUNCELLIMALG extract cell images and calls alg with it
-%   It returns with a full scatter image not cell image
-% alg : function handler for the cell-image based scatter estimator
-% image : the modulated image
-% modFF : flood-field image of the modulator
+%   It returns a full-scale scatter image not a cell image
+% alg          : function handler for the cell-image based scatter estimator
+% modProj      : the modulated image
+% modFF        : flood-field image of the modulator
 % cellWinCache : cache containing precalculated positioned windows for each
 % cell
 
